2e98bfee6d4033d5a7fe8bb261407fc0e660a6b1
Kmitt91 <71717577+Kmitt91@users.noreply.github.com>
2025-11-29 14:18:46 +0100

    blocking fixed

--- project-kmitt/godot_knight/player_components/gd_script/blocking_system.gd
+++ project-kmitt/godot_knight/player_components/gd_script/blocking_system.gd
@@ -1,3 +1,4 @@
+#currently only anim
 extends Node
 
 @export var player : CharacterBody3D
@@ -5,9 +6,12 @@ extends Node
 var timer_block_one_shot : bool = false
 var block_pos_sound : bool = false
 #@onready var block_spark = preload("res://VFX/VFX_Magic/Blood_Scroll/Blood_Scroll_block.tscn")
-@onready var shield_area : CollisionShape3D = shield.get_node("Area3DBlock/CollisionShape3D")
-@onready var shield = $godot_knight
+#@onready var shield_area : CollisionShape3D = shield.get_node("Area3DBlock/CollisionShape3D")
+#@onready var shield = $godot_knight
 const ACCELERATION : float = 5.0
+
+func _physics_process(delta):
+	block(delta)
 
 func block(delta):
 	if !player.roll and !player.jump and !player.magic and !player.attack:
@@ -16,13 +20,13 @@ func block(delta):
 		if Input.is_action_pressed("L1") or Input.is_action_pressed("LEFT_MOUSE"):
 			player.anim_tree.set("parameters/" + player.state + "/block/blend_amount", lerp(float(player.anim_tree.get("parameters/" + player.state + "/block/blend_amount")), 1.0, delta * ACCELERATION * 2.0))
 			player.disabled_movement = true
-			shield.set_scale(lerp(shield.get_scale(), Vector3(1.2,1.2,1.2), delta * 10.0))
+			#shield.set_scale(lerp(shield.get_scale(), Vector3(1.2,1.2,1.2), delta * 10.0))
 			#block
-			if player.nim_tree.get("parameters/" + player.state + "/block/blend_amount") > 0.92 and !player.anim_tree.get("parameters/" + player.state + "/stagger/active"):
+			if player.anim_tree.get("parameters/" + player.state + "/block/blend_amount") > 0.92 and !player.anim_tree.get("parameters/" + player.state + "/stagger/active"):
 				player.blocking = true
 				if timer_block.is_stopped() and !timer_block_one_shot:
 					timer_block.start(0)
-					shield_area.disabled = false
+					#shield_area.disabled = false
 					#back_area.disabled = false
 			#if anim_tree.get("parameters/" + state + "/block/blend_amount") < 0.1:
 				
@@ -30,9 +34,9 @@ func block(delta):
 		elif player.anim_tree.get("parameters/" + player.state + "/block/blend_amount") > 0.0:
 			player.blocking = false
 			player.anim_tree.set("parameters/" + player.state + "/block/blend_amount", lerp(float(player.anim_tree.get("parameters/" + player.state + "/block/blend_amount")), 0.0, delta * ACCELERATION))
-			shield_area.disabled = true
+			#shield_area.disabled = true
 			#back_area.disabled = true
-			shield.set_scale(lerp(shield.get_scale(), Vector3(1,1,1), delta * 10.0))
+			#shield.set_scale(lerp(shield.get_scale(), Vector3(1,1,1), delta * 10.0))
 			
 			if !timer_block.is_stopped() or timer_block_one_shot:
 				timer_block.stop()
@@ -48,10 +52,10 @@ func block(delta):
 
 func _on_timer_block_timeout():
 	timer_block_one_shot = true
-	shield_area.disabled = true
+	#shield_area.disabled = true
 
-func _on_area_3d_block_area_entered(area):
-	pass
+#func _on_area_3d_block_area_entered(area):
+#	pass
 	#if area.is_in_group("WEAPON"):
 		#hit_shake_camera(0.2,0.15)
 		#shield.add_child(VFX.magic_vfx_instance("Damage_Scroll"))
